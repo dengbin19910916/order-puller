3b10d061f0b29e9feb2add3e625cb678829d0588
dengbin <dengbin@meicloud.com>
2019-01-11 17:43:03 +0800

    重构

---
+++ vip-order-puller/src/main/java/com/willowleaf/VipOrderPullerApplication.java
@@ -0,0 +1,12 @@
+package com.willowleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipOrderPullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VipOrderPullerApplication.class, args);
+    }
+}
---
+++ vip-order-puller/src/main/java/com/willowleaf/vippull/VipOrderPuller.java
@@ -0,0 +1,39 @@
+package com.willowleaf.vippull;
+
+import com.willowleaf.orderpull.core.OrderPuller;
+import com.willowleaf.orderpull.core.TimeInterval;
+import com.willowleaf.orderpull.core.model.Goods;
+import com.willowleaf.orderpull.core.model.Order;
+import com.willowleaf.orderpull.core.model.Platform;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class VipOrderPuller extends OrderPuller {
+
+    @Override
+    protected List<Order> pull(TimeInterval timeInterval) {
+        System.out.println("拉取唯品会订单，开始时间：" + timeInterval.getStartTime() + "，结束时间：" + timeInterval.getEndTime() + "。");
+        Order order1 = new Order();
+        order1.setId(1000000001L);
+        Goods goods1 = new Goods();
+        goods1.setId(2000000001L);
+        order1.setGoods(Collections.singletonList(goods1));
+
+        Order order2 = new Order();
+        order2.setId(1000000002L);
+        Goods goods2 = new Goods();
+        goods2.setId(2000000002L);
+        order2.setGoods(Collections.singletonList(goods2));
+
+        return Arrays.asList(order1, order2);
+    }
+
+    @Override
+    protected Platform getPlatform() {
+        return Platform.VIP;
+    }
+}
