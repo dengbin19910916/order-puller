c5c845d71116eaacb676a29ec6e3348dcd8e2fb0
dengbin <dengbin@meicloud.com>
2019-01-11 17:24:18 +0800

    重构

---
+++ jd-order-puller/src/main/java/com/willowleaf/jdpull/JdOrderPuller.java
@@ -0,0 +1,39 @@
+package com.willowleaf.jdpull;
+
+import com.willowleaf.orderpull.core.OrderPuller;
+import com.willowleaf.orderpull.core.TimeInterval;
+import com.willowleaf.orderpull.core.model.Goods;
+import com.willowleaf.orderpull.core.model.Order;
+import com.willowleaf.orderpull.core.model.Platform;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class JdOrderPuller extends OrderPuller {
+
+    @Override
+    protected List<Order> pull(TimeInterval timeInterval) {
+        System.out.println("拉取京东订单，开始时间：" + timeInterval.getStartTime() + "，结束时间：" + timeInterval.getEndTime() + "。");
+        Order order1 = new Order();
+        order1.setId(1000000000001L);
+        Goods goods1 = new Goods();
+        goods1.setId(2000000000001L);
+        order1.setGoods(Collections.singletonList(goods1));
+
+        Order order2 = new Order();
+        order2.setId(1000000000002L);
+        Goods goods2 = new Goods();
+        goods2.setId(2000000000002L);
+        order2.setGoods(Collections.singletonList(goods2));
+
+        return Arrays.asList(order1, order2);
+    }
+
+    @Override
+    protected Platform getPlatform() {
+        return Platform.ALIBABA;
+    }
+}
