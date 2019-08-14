5abab68a0a00ba390261e64b09c695359528e1ea
Dengbin <dengbin@winnermedical.com>
2019-08-14 23:57:02 +0800

    移除无用的文件

--- order-puller-core/src/main/java/com/willowleaf/orderpull/core/OrderPusher.java
+++ order-puller-core/src/main/java/com/willowleaf/orderpull/core/OrderPusher.java
@@ -1,8 +1,13 @@
 package com.willowleaf.orderpull.core;
 
 import com.willowleaf.orderpull.core.model.Order;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -11,8 +16,9 @@ import static com.willowleaf.orderpull.core.JobAutoConfiguration.ORDER_QUEUE_NAM
 /**
  * 将标准订单推送至RabbitMQ。
  */
+@Slf4j
 @Component
-public class OrderPusher {
+public class OrderPusher implements RabbitTemplate.ConfirmCallback, RabbitTemplate.ReturnCallback {
 
     private final AmqpTemplate amqpTemplate;
 
@@ -21,8 +27,19 @@ public class OrderPusher {
     }
 
     void push(List<Order> orders) {
-        if (orders.size() > 0) {
+        if (!ObjectUtils.isEmpty(orders)) {
             amqpTemplate.convertAndSend(ORDER_QUEUE_NAME, orders);
         }
+    }
+
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        log.error("RabbitMQ confirm error. CorrelationData: {}\nAck:{}\nCause:{}", correlationData, ack, cause);
+    }
+
+    @Override
+    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+        log.error("RabbitMQ returned message error. Message: {}\nReplyCode: {}\nReplyText: {}\nExchange: {}\nRoutingKey: {}",
+                message, replyCode, replyText, exchange, routingKey);
     }
 }
